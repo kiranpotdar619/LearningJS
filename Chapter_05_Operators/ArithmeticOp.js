de8a1d826b3b094974942182861a1c15b4490465
kiranpotdar619 <kiranpotdar619@gmail.com>
2026-07-16 08:09:35 +0530

    Add Chapter_05_Operators - Arithmetic and Assignment operators
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Chapter_05_Operators/ArithmeticOp.js
@@ -0,0 +1,18 @@
+
+let a=34;
+let b=22;
+
+let sum = a+b;
+let sub = a-b;
+let mul = a*b;
+let div = a/b;
+let mod = a%b;
+
+console.log(sum);
+console.log(sub);
+console.log(mul);
+console.log(div);
+console.log(mod);
+
+//Exponential operator
+console.log(2**3);
